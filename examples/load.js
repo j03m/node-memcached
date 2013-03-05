53bf4ecbdda6381aec6671e2adcf8dc8a0215d57
Joe Mordetsky <jmordetsky@zynga.com>
2013-03-05 12:12:32 -0500

    syncing loader from mcmux branch.

--- examples/load.js
+++ examples/load.js
@@ -8,14 +8,14 @@
 
 var Membase = require('../');
 var membases = [];
-var numClients = 5;
+var numClients = 1;
 for(var i=0;i<numClients;i++){
     	//membases.push(new Membase("127.0.0.1:11211", {poolSize:10}));
 	membases.push(new Membase("10.80.81.242:11211", {poolSize:10}));
 }
 
 var start = new Date().getTime();
-for (var i =0;i<50;i++){
+for (var i =0;i<500;i++){
     setInterval(function(){
         go();
     },0);
@@ -52,8 +52,8 @@ function go(){
                     throw new Error(err);
                 }
                 if (key!=result) {
-
-                    //  throw new Error("ru oh");
+		    console.log("*BAD: key:" + key + " result: " + result);
+                    throw new Error("ru oh");
                 }
                 if (count>1000){
                     sum();
